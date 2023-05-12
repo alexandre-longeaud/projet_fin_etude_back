e2a6c34b99b9e23dd65c61d308ce644b4282eeb4
alexandre-longeaud <alexandre.longeaud08@gmail.com>
2023-05-12 11:08:43 +0200

    mise à jour controller

--- src/Controller/Api/PictureController.php
+++ src/Controller/Api/PictureController.php
@@ -16,6 +16,7 @@ class PictureController extends AbstractController
   
      /**
      * Affiche les 30 images les plus récente en page d'accueil / Display the 30 most recents pictures on homepage
+     * 
      * @Route("/pictures/home", name="app_api_picture_browseByCreatedAt", methods={"GET"})
      */
     public function browseByCreatedAt(PictureRepository $pictureRepository): JsonResponse
@@ -26,19 +27,21 @@ class PictureController extends AbstractController
 
      /**
      * Affiche l'image selectionnée / Display the selected picture
+     * 
      * @Route("/pictures/{id}", name="app_api_picture_read", requirements={"id"="\d+"}, methods={"GET"})
      */
     public function read($id, PictureRepository $pictureRepository): JsonResponse
     {
         $picture = $pictureRepository->find($id);
 
-        if ($picture === null){return $this->json("messages d'erreur",Response::HTTP_NOT_FOUND);}
+        if ($picture === null){return $this->json("image inexistant",Response::HTTP_NOT_FOUND);}
 
         return $this->json($picture, 200, [], ["groups"=>"picture"]);
     }
 
       /**
      * Affiche les 30 images les plus likées / Display the 30 most liked pictures
+     * 
      * @Route("/pictures/liked", name="app_api_picture_browseMostLiked", methods={"GET"})
      */
     public function browseMostLiked(PictureRepository $pictureRepository): JsonResponse
@@ -49,6 +52,7 @@ class PictureController extends AbstractController
 
       /**
      * Affiche les 30 images les plus regardées/cliquées / Display the 30 most clicked pictures
+     * 
      * @Route("/pictures/clicked", name="app_api_picture_browseMostClicked", methods={"GET"})
      */
     public function browseMostClicked(PictureRepository $pictureRepository): JsonResponse
@@ -59,6 +63,7 @@ class PictureController extends AbstractController
 
      /**
      * Affiche les 30 images les plus commentées / Display the 30 most commented pictures
+     * 
      * @Route("/pictures/reviewed", name="app_api_pictures_browseMostReviewed", methods={"GET"})
      */
     public function browseMostReviewed(PictureRepository $pictureRepository): JsonResponse
