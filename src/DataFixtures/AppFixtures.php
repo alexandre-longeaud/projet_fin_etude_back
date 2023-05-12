bc5ac85c1b0285cb654d6a887023c1e5b79f5dfd
Etudiant O'clock <christophecumbo@gmail.com>
2023-05-12 12:15:06 +0200

    BE fixture fix: relation user avec commentaires et images

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -51,10 +51,99 @@ class AppFixtures extends Fixture
 
         // on tronque les tables
         $this->truncate();
+
+
+                // liste de Users
+
+            // administrateurs
+
+            $userBen = new User();
+            $userBen->setPseudo('Benoit-R');
+            $userBen->setMail('benoit@benoit.com');
+            $roleAdmin = new Role();
+            $roleAdmin->setName('ROLE_ADMIN');
+            $roleAdmin->setCreatedAt(new DateTime('now'));
+            $userBen->setRole($roleAdmin);
+            $userBen->setPassword('$2y$13$QzAdagb9dwOGrkFaVQYYbOzuZypCHfE2bRnx/QTuJqInMrM1JLmaK');
+            $userBen->setBio('Benoît Rolet, product owner et administrateur de Maisterpiece.com');
+            $userBen->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050V5MTX9Q-8c2f44989391-512');
+            $userBen->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleAdmin);
+            $manager->persist($userBen);
+    
+    
+            $userNico = new User();
+            $userNico->setPseudo('Nico-C');
+            $userNico->setMail('nico@nico.com');
+            $roleAdmin->setCreatedAt(new DateTime('now'));
+            $userNico->setRole($roleAdmin);
+            $userNico->setPassword('$2y$13$pDd02nUT43D6AGyln8D/0eQQdxdSXBMYeOiRMOz2Va.sVYwMssv7u');
+            $userNico->setBio('Nicolas Caron, lead developer frontend et administrateur de Maisterpiece.com');
+            $userNico->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050U6DK5V1-3427f3bfa239-512');
+            $userNico->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleAdmin);
+            $manager->persist($userNico);
+    
+    
+            $userAurelie = new User();
+            $userAurelie->setPseudo('Aurelie-S');
+            $userAurelie->setMail('aurelie@aurelie.com');
+            $roleAdmin->setCreatedAt(new DateTime('now'));
+            $userAurelie->setRole($roleAdmin);
+            $userAurelie->setPassword('$2y$13$QGA6t.or2IgD6pkdICiSTOorI0bIZUGu1yjntOKuQaCZHlBtzO3b6');
+            $userAurelie->setBio('Aurelie Simonneau, Scrum Master et administrateur de Maisterpiece.com');
+            $userAurelie->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050MEYMPJA-d4f976fc0f58-512');
+            $userAurelie->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleAdmin);
+            $manager->persist($userAurelie);
+            
+    
+            $userAlex = new User();
+            $userAlex->setPseudo('Alex-L');
+            $userAlex->setMail('alex@alex.com');
+            $roleAdmin->setCreatedAt(new DateTime('now'));
+            $userAlex->setRole($roleAdmin);
+            $userAlex->setPassword('$2y$13$UyNGRncDul0e2mObEzj7gu.4GvtzfEgVWbQj2N.hefBn5pNi3ITE6');
+            $userAlex->setBio('Alexandre Longeaud, Git Master et administrateur de Maisterpiece.com');
+            $userAlex->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050ZEZEE1G-g00bdeff674d-512');
+            $userAlex->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleAdmin);
+            $manager->persist($userAlex);
+    
+    
+            $userChris = new User();
+            $userChris->setPseudo('Chris-C');
+            $userChris->setMail('christophe@christophe.com');
+            $roleAdmin->setCreatedAt(new DateTime('now'));
+            $userChris->setRole($roleAdmin);
+            $userChris->setPassword('$2y$13$V2rUi5jWZj9Itzi2OcQB3uh6y/D6XXT5nKLvZjybAieihfUNoEYXi');
+            $userChris->setBio('Christophe Cumbo, lead developer backend et administrateur de Maisterpiece.com');
+            $userChris->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U051F78D99P-7c135f7c743f-512');
+            $userChris->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleAdmin);
+            $manager->persist($userChris);
+    
+    
+                //utilisateur membre
+    
+            $userMembre = new User();
+            $userMembre->setPseudo('User-1');
+            $userMembre->setMail('user1@user1.com');
+            $roleUser = new Role();
+            $roleUser->setName('ROLE_USER');
+            $roleUser->setCreatedAt(new DateTime('now'));
+            $userMembre->setRole($roleUser);
+            $userMembre->setPassword('$2y$13$1l2Gv/9G5caLbjuOIz5VCeAYDhZHMM6yFoDFlny0ys2wynA2teh2m');
+            $userMembre->setBio('User1, inscrit sur Maisterpiece et membre actif sur le site');
+            $userMembre->setAvatar('https://www.zupimages.net/up/23/18/lmmr.jpg');
+            $userMembre->setCreatedAt(new DateTime('now'));
+            $manager->persist($roleUser);
+            $manager->persist($userMembre);
+    
         
         // * je constitue une liste des IA
         $allIa = [];
-        for ($i=0; $i < 5; $i++) {
+        for ($i=1; $i < 6; $i++) {
             // 1. créer l'objet
             $newIa = new Ia();
             // 2. on met à jour les propriétés
@@ -72,12 +161,14 @@ class AppFixtures extends Fixture
         for ($i=0; $i < 31; $i++) {
             // 1. créer un objet Picture
             $newPicture = new Picture();
+           
 
             // 2. remplire les propriétés de mon nouvel objet
             $newPicture->setPrompt("mon super prompt #" . $i);
             $newPicture->setUrl("https://www.zupimages.net/up/23/18/8ptc.jpg");
             $newPicture->setNbClick(mt_rand(0, 99));
             $newPicture->setCreatedAt(new DateTime('now'));
+            $newPicture->setUser($userMembre);
 
             $randomIndexIa = mt_rand(0, count($allIa)-1);
             $randomIa = $allIa[$randomIndexIa];
@@ -86,6 +177,7 @@ class AppFixtures extends Fixture
             $newReview = new Review();
 
             $newReview->setContent("super image #" . $i);
+            $newReview->setUser($userMembre);
 
             $newReview->setCreatedAt(new DateTime('now'));
             $newPicture->addReview($newReview);
@@ -93,92 +185,6 @@ class AppFixtures extends Fixture
             $manager->persist($newPicture);
         }
 
-        // liste de Users
-
-            // administrateurs
-
-        $userBen = new User();
-        $userBen->setPseudo('Benoit-R');
-        $userBen->setMail('benoit@benoit.com');
-        $roleAdmin = new Role();
-        $roleAdmin->setName('ROLE_ADMIN');
-        $roleAdmin->setCreatedAt(new DateTime('now'));
-        $userBen->setRole($roleAdmin);
-        $userBen->setPassword('$2y$13$QzAdagb9dwOGrkFaVQYYbOzuZypCHfE2bRnx/QTuJqInMrM1JLmaK');
-        $userBen->setBio('Benoît Rolet, product owner et administrateur de Maisterpiece.com');
-        $userBen->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050V5MTX9Q-8c2f44989391-512');
-        $userBen->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleAdmin);
-        $manager->persist($userBen);
-
-
-        $userNico = new User();
-        $userNico->setPseudo('Nico-C');
-        $userNico->setMail('nico@nico.com');
-        $roleAdmin->setCreatedAt(new DateTime('now'));
-        $userNico->setRole($roleAdmin);
-        $userNico->setPassword('$2y$13$pDd02nUT43D6AGyln8D/0eQQdxdSXBMYeOiRMOz2Va.sVYwMssv7u');
-        $userNico->setBio('Nicolas Caron, lead developer frontend et administrateur de Maisterpiece.com');
-        $userNico->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050U6DK5V1-3427f3bfa239-512');
-        $userNico->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleAdmin);
-        $manager->persist($userNico);
-
-
-        $userAurelie = new User();
-        $userAurelie->setPseudo('Aurelie-S');
-        $userAurelie->setMail('aurelie@aurelie.com');
-        $roleAdmin->setCreatedAt(new DateTime('now'));
-        $userAurelie->setRole($roleAdmin);
-        $userAurelie->setPassword('$2y$13$QGA6t.or2IgD6pkdICiSTOorI0bIZUGu1yjntOKuQaCZHlBtzO3b6');
-        $userAurelie->setBio('Aurelie Simonneau, Scrum Master et administrateur de Maisterpiece.com');
-        $userAurelie->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050MEYMPJA-d4f976fc0f58-512');
-        $userAurelie->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleAdmin);
-        $manager->persist($userAurelie);
-        
-
-        $userAlex = new User();
-        $userAlex->setPseudo('Alex-L');
-        $userAlex->setMail('alex@alex.com');
-        $roleAdmin->setCreatedAt(new DateTime('now'));
-        $userAlex->setRole($roleAdmin);
-        $userAlex->setPassword('$2y$13$UyNGRncDul0e2mObEzj7gu.4GvtzfEgVWbQj2N.hefBn5pNi3ITE6');
-        $userAlex->setBio('Alexandre Longeaud, Git Master et administrateur de Maisterpiece.com');
-        $userAlex->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U050ZEZEE1G-g00bdeff674d-512');
-        $userAlex->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleAdmin);
-        $manager->persist($userAlex);
-
-
-        $userChris = new User();
-        $userChris->setPseudo('Chris-C');
-        $userChris->setMail('christophe@christophe.com');
-        $roleAdmin->setCreatedAt(new DateTime('now'));
-        $userChris->setRole($roleAdmin);
-        $userChris->setPassword('$2y$13$V2rUi5jWZj9Itzi2OcQB3uh6y/D6XXT5nKLvZjybAieihfUNoEYXi');
-        $userChris->setBio('Christophe Cumbo, lead developer backend et administrateur de Maisterpiece.com');
-        $userChris->setAvatar('https://ca.slack-edge.com/T051G8W6UAC-U051F78D99P-7c135f7c743f-512');
-        $userChris->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleAdmin);
-        $manager->persist($userChris);
-
-
-            //utilisateur membre
-
-        $userMembre = new User();
-        $userMembre->setPseudo('User-1');
-        $userMembre->setMail('user1@user1.com');
-        $roleUser = new Role();
-        $roleUser->setName('ROLE_USER');
-        $roleUser->setCreatedAt(new DateTime('now'));
-        $userMembre->setRole($roleUser);
-        $userMembre->setPassword('$2y$13$1l2Gv/9G5caLbjuOIz5VCeAYDhZHMM6yFoDFlny0ys2wynA2teh2m');
-        $userMembre->setBio('User1, inscrit sur Maisterpiece et membre actif sur le site');
-        $userMembre->setAvatar('https://www.zupimages.net/up/23/18/lmmr.jpg');
-        $userMembre->setCreatedAt(new DateTime('now'));
-        $manager->persist($roleUser);
-        $manager->persist($userMembre);
 
             
 
