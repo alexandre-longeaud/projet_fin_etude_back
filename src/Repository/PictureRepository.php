353e193e2bc226be09a4dd12d9d15686d3bb9099
Etudiant O'clock <christophecumbo@gmail.com>
2023-05-15 16:18:08 +0200

    BE find by Clic

--- src/Repository/PictureRepository.php
+++ src/Repository/PictureRepository.php
@@ -83,7 +83,7 @@ class PictureRepository extends ServiceEntityRepository
     * retourne les 30 images les plus vues 
     */
 
-    public function findPictureByViews()
+    public function findPictureByNbClick()
     {
         // recupere la connexion à la bdd
         $conn = $this->getEntityManager()->getConnection();
@@ -91,9 +91,9 @@ class PictureRepository extends ServiceEntityRepository
         // la requete qui correspond à cherche les films, trie les aléatoirement et garde en qu'un.
         // movie m correspond à la table movie et pendant la requête on peut y faire référence avec la lettre m
         $sql = '
-            SELECT * FROM `picture`
-            ORDER BY RAND() 
-            LIMIT 30
+        SELECT * FROM `picture`
+        ORDER BY nb_click DESC
+        LIMIT 30            
             ';
 
         // on execute la requête
