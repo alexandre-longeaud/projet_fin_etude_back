170c27733584eec102b6c9ddb19a820a5d078b31
Etudiant O'clock <christophecumbo@gmail.com>
2023-05-16 16:04:39 +0200

    querry builder filtre

--- src/Repository/PictureRepository.php
+++ src/Repository/PictureRepository.php
@@ -55,26 +55,16 @@ class PictureRepository extends ServiceEntityRepository
     * retourne les 30 images les plus likées 
     */
 
-    public function findPictureByLikes()
+    public function findPictureByLikes(): array
     {
-        // recupere la connexion à la bdd
-        $conn = $this->getEntityManager()->getConnection();
-
-        // la requete qui correspond à cherche les films, trie les aléatoirement et garde en qu'un.
-        // movie m correspond à la table movie et pendant la requête on peut y faire référence avec la lettre m
-        $sql = '
-            SELECT * FROM `picture`
-            ORDER BY RAND() 
-            LIMIT 30
-            ';
-
-        // on execute la requête
-        $stmt = $conn->prepare($sql);
-        $resultSet = $stmt->executeQuery();
-
-
-        // returns an array
-        return $resultSet->fetchAssociative();
+        return $this->createQueryBuilder('picture')
+            ->select('picture, COUNT(l.id) AS nombre_like')
+            ->leftJoin('picture.likes', 'l')
+            ->groupBy('picture.id')
+            ->orderBy('nombre_like', 'DESC')
+            ->setMaxResults(30)
+            ->getQuery()
+            ->getResult();
     }
    /**
     * retourne les 30 images les plus vues 
@@ -82,51 +72,23 @@ class PictureRepository extends ServiceEntityRepository
 
     public function findPicturerByNbClic()
     {
-        // recupere la connexion à la bdd
-        $conn = $this->getEntityManager()->getConnection();
-
-        // la requete qui correspond à cherche les films, trie les aléatoirement et garde en qu'un.
-        // movie m correspond à la table movie et pendant la requête on peut y faire référence avec la lettre m
-        $sql = '
-        SELECT * FROM `picture`
-        ORDER BY nb_click DESC
-        LIMIT 30           
-         ';
-
-        // on execute la requête
-        $stmt = $conn->prepare($sql);
-        $resultSet = $stmt->executeQuery();
-
-
-        // returns an array
-        return $resultSet->fetchAssociative();
+        return $this->createQueryBuilder('picture')
+            ->orderBy('picture.nbClick', 'DESC')
+            ->setMaxResults(30)
+            ->getQuery()
+            ->getResult();
     }
 
     public function findByPictureMostReview()
     {
-        // recupere la connexion à la bdd
-        $conn = $this->getEntityManager()->getConnection();
-
-        // la requete qui correspond à cherche les films, trie les aléatoirement et garde en qu'un.
-        // movie m correspond à la table movie et pendant la requête on peut y faire référence avec la lettre m
-        $sql = '
-        SELECT picture.*, user.*,COUNT(review.id) AS nombre_commentaires 
-        FROM picture
-        INNER JOIN review ON picture.id = review.picture_id
-        INNER JOIN user ON picture.user_id = user.id
-        GROUP BY picture.id
-        ORDER BY nombre_commentaires DESC
-        LIMIT 30;
-            ';
-
-        // on execute la requête
-        $stmt = $conn->prepare($sql);
-        $resultSet = $stmt->executeQuery();
-
-
-        // returns an array
-        return $resultSet->fetchAssociative();
-    }
+        return $this->createQueryBuilder('picture')
+        ->select('picture, COUNT(r.id) AS nombre_review')
+        ->leftJoin('picture.reviews', 'r')
+        ->groupBy('picture.id')
+        ->orderBy('nombre_review', 'DESC')
+        ->setMaxResults(30)
+        ->getQuery()
+        ->getResult();   }
 //    /**
 //     * @return Picture[] Returns an array of Picture objects
 //     */
