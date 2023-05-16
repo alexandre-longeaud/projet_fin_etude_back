af1e1e62c24e1967bffc2c8edde8924c4f2dfcc2
Etudiant O'clock <christophecumbo@gmail.com>
2023-05-16 13:42:10 +0200

    fix user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -72,6 +73,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $likes;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Picture::class, mappedBy="user")
+     */
+    private $picture;
 
     public function __construct()
     {
