7f83df3078d4af14945308240efc0dd7a2bd33fd
alexandre-longeaud <alexandre.longeaud08@gmail.com>
2023-05-23 15:19:05 +0200

    validation des champs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,10 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
 
 
 /**
